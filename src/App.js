66b964fbc076a2fc53120ccbb781c3102c06a3b4
SohyeonP <hidhen9714@naver.com>
2020-05-20 09:51:39 +0900

    react 과제

--- src/App.js
+++ src/App.js
@@ -1,32 +1,84 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import Clock from './Clock';
-import Userlist from './Userlist';
+import { TextField, Typography, Paper } from '@material-ui/core';
+import { KeyboardTimePicker, KeyboardDatePicker } from '@material-ui/pickers';
+import moment from 'moment';
 
-class App extends React.Component{
-  render(){
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <Clock color = "white"/>
-        <Userlist />
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      title : "",
+      content : "",
+      startDate : new Date(),
+      startTime : new Date(),
+      endDate : new Date(),
+      endTime : new Date(),
+      //items : [],
+    }
+  }
+
+  render() {
+    const { title,content,startDate,startTime,endDate,endTime } = this.state;
+    console.log(title, content, moment(startDate).format("LL"), moment(startTime).format("LT"), moment(endDate).format("LL"), moment(endTime).format("LT"));
+    return (
+      <div className="app">
+        <div className="header">TODO LIST</div>
+        <Paper className="input-area" variant="outlined"style={{padding: '10px'}} >
+          <TextField onChange={(value) => this.setState({ title : value.currentTarget.value})} label="제목" size="medium" margin="none" fullWidth required/>
+          <TextField onChange={(value) => this.setState({ content : value.currentTarget.value})} label="상세내용" size="medium" margin="none" fullWidth multiline/>
+          <KeyboardDatePicker 
+            disableToolbar
+            variant="inline"
+            format="yyyy/MM/DD"
+            margin="normal"
+            label="시작 예정일"
+            value={startDate}
+            onChange={(value) => this.setState({ startDate : value._d})}
+            style={{width: '50%'}}
+            KeyboardButtonProps={{ 'aria-label' : 'change date',}}
+          />
+          <KeyboardTimePicker 
+            margin="normal"
+            label="시작시간"
+            variant="inline"
+            value={startTime}
+            onChange={(value)=> this.setState({ startTime : value._d})}
+            style={{width: '50%'}}
+            KeyboardButtonProps={{
+              'aria-label' : 'change time',
+            }}
+          />
+          <KeyboardDatePicker 
+            disableToolbar
+            variant="inline"
+            format="yyyy/MM/DD"
+            margin="normal"
+            label="종료 예정일"
+            value={endDate}
+            onChange={(value) => this.setState({ endDate : value._d})}
+            style={{width: '50%'}}
+            KeyboardButtonProps={{ 'aria-label' : 'change date',}}
+          />
+          <KeyboardTimePicker 
+            margin="normal"
+            label="종료시간"
+            variant="inline"
+            value={endTime}
+            onChange={(value)=> this.setState({ endTime : value._d})}
+            style={{width: '50%'}}
+            KeyboardButtonProps={{
+              'aria-label' : 'change time',
+            }}
+          />
+        </Paper>
+        <div className="list-area">리스트 영역</div>
+          <Typography variant="body2" color="textSecondary" align="center">
+            {'Copyright @ akdom ' + new Date().getFullYear() + '.'}
+          </Typography>
+      </div>
     );
   }
-  
 }
+
 export default App;
